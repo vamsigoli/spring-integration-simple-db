28d2f690b7b231b525eb243228644713e19af739
vamsi goli <vamsi.goli@gmail.com>
2023-12-22 18:00:19 -0500

    modified to have outbound channel

--- src/main/java/com/vamsi/simpledb/SimpleDbIntegrationApplication.java
+++ src/main/java/com/vamsi/simpledb/SimpleDbIntegrationApplication.java
@@ -3,19 +3,18 @@ package com.vamsi.simpledb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.core.GenericHandler;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.PollerFactory;
+import org.springframework.integration.jdbc.JdbcMessageHandler;
 import org.springframework.integration.jdbc.JdbcPollingChannelAdapter;
+import org.springframework.integration.jdbc.MessagePreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Duration;
-import java.util.List;
 
 @SpringBootApplication
 public class SimpleDbIntegrationApplication {
@@ -30,30 +29,55 @@ public class SimpleDbIntegrationApplication {
         var jdbcPollingChannelAdapter = new JdbcPollingChannelAdapter(dataSource,
                 "SELECT * FROM CUSTOMER where processed = false");
         jdbcPollingChannelAdapter.setRowMapper(customerRowMapper);
-        jdbcPollingChannelAdapter.setUpdatePerRow(true);
-        jdbcPollingChannelAdapter.setUpdateSql("update customer set processed = true where id = :id");
-        jdbcPollingChannelAdapter.setUpdateSqlParameterSourceFactory(input -> {
-            if (input instanceof Customer customer) {
-                return new MapSqlParameterSource("id", customer.id());
-            } else {
-                return null;
-            }
-        });
+        //the below is not optimal because we are updating the record as soon as we get as part of inbound flow.
+        //it should be done as part of the outbound flow after the processing has been done in the handle methods.
+//        jdbcPollingChannelAdapter.setUpdatePerRow(true);
+//        jdbcPollingChannelAdapter.setUpdateSql("update customer set processed = true where id = :id");
+//        jdbcPollingChannelAdapter.setUpdateSqlParameterSourceFactory(input -> {
+//            if (input instanceof Customer customer) {
+//                return new MapSqlParameterSource("id", customer.id());
+//            } else {
+//                return null;
+//            }
+//        });
         return jdbcPollingChannelAdapter;
     }
 
     @Bean
-    IntegrationFlow jdbcInboundFlow(JdbcPollingChannelAdapter jdbcPollingChannelAdapter) {
+    IntegrationFlow jdbcFlow(JdbcPollingChannelAdapter jdbcPollingChannelAdapter,
+                             JdbcMessageHandler outboundHandler) {
         return IntegrationFlow.from(jdbcPollingChannelAdapter,
                         poller -> poller.poller(pm -> PollerFactory.fixedRate(Duration.ofSeconds(1))))
-                .handle((GenericHandler<List<Customer>>) (payload, headers) -> {
+                .split()
+                .handle((payload, headers) -> {
                     System.out.println(payload);
                     headers.forEach((k, v) -> System.out.println(k + "=" + v));
-                    return null;
+                    return payload;
                 })
+                .aggregate()
+                //if the aggregate call is present above, all the individual customer records available after split
+                //pass through the handle method which prints the payload and return the same payload
+                //each individual payload represents one element in the collection returned by jdbcPollingChannelAdapter
+                //they are combined and one collection of customers by aggregate method and is sent to the outboundHandler
+                //if you see the javadoc of JdbcMessageHandler, it says that if it receives an Iterable, it uses batch update
+                //and if it receives individual object, it uses non batch update functionality. so the above aggregate
+                //improves performance by using batch update
+                .handle(outboundHandler)
                 .get();
     }
 
+    @Bean
+    JdbcMessageHandler jdbcMessageHandler(DataSource dataSource) {
+        JdbcMessageHandler jdbcMessageHandler = new JdbcMessageHandler(dataSource,
+                "UPDATE CUSTOMER SET processed = true where id = ?");
+        MessagePreparedStatementSetter messagePreparedStatementSetter = (ps,message) -> {
+            Customer customer = (Customer) message.getPayload();
+            ps.setInt(1, customer.id());
+        };
+        jdbcMessageHandler.setPreparedStatementSetter(messagePreparedStatementSetter);
+
+        return jdbcMessageHandler;
+    }
 
     @Component
     static class CustomerRowMapper implements RowMapper<Customer> {
@@ -63,7 +87,5 @@ public class SimpleDbIntegrationApplication {
         }
     }
 
-    record Customer(Integer id, String name) {
-
-    }
+    record Customer(Integer id, String name) {}
 }
